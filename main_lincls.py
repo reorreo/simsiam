14a328f796c015381142d9a2a46d62d75da96d9a
reorreo <reocci@gmail.com>
2023-08-29 15:22:26 +0900

    test

--- main_lincls.py
+++ main_lincls.py
@@ -261,6 +261,7 @@ def main_worker(gpu, ngpus_per_node, args):
     valdir = os.path.join(args.data, 'val')
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
+    print(traindir)
 
     train_dataset = datasets.ImageFolder(
         traindir,
