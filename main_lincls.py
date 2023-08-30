8c09f2ef71d7515f1a89a74570b85ff77c1d5ec5
reorreo <reocci@gmail.com>
2023-08-30 14:17:42 +0900

    エポック数変更

--- main_lincls.py
+++ main_lincls.py
@@ -42,7 +42,7 @@ parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet50',
 parser.add_argument('-j', '--workers', default=32, type=int, metavar='N',
                     help='number of data loading workers (default: 32)')
 #epochs
-parser.add_argument('--epochs', default=5, type=int, metavar='N',
+parser.add_argument('--epochs', default=90, type=int, metavar='N',
                     help='number of total epochs to run')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
