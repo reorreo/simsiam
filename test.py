3202d0e6afe7ac91e896ccd9164980c843b4b573
reorreo <reocci@gmail.com>
2023-08-31 12:26:16 +0900

    test

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 for i in range(6):
     test_log=open('./acc/test+_acc.csv','a') 
-    test_log.write(' {:4d},{top1.avg:.3f},{top5.avg:7.3f}\n'
+    test_log.write(' {:4d},{:.3f},{:7.3f}\n'
             .format(i, i+1, i*(1+1)))
     test_log.close()
